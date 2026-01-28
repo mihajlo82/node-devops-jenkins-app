6c311aa0ec0d923d842ee99b217ad2ff292bd071
Mihajlo <dimitricmihajlo995@gmail.com>
2026-01-28 17:31:38 +0100

    small change 12

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const VERSION = process.env.APP_VERSION || "local";
 
 app.get("/", (req, res) => {
   res.json({
-    status: "okkgg",
+    status: "okkgghj",
     env: ENV,
     version: VERSION
   });
